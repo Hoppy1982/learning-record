3e79d9d6b18a818695e49f301b8509618b572931
mark hopcraft <mark82hopcraft@gmail.com>
2018-07-25 17:52:23 +0100

    initial commit

---
+++ scripts/build-ejs.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const fs = require('fs')
+const ejs = require('ejs')
+
+const ejsDir = path.join(__dirname, '/../client/src/views')
+const htmlDir = path.join(__dirname, '/../client/built/html')
+
+fs.readdir(ejsDir, (err, filenames) => {
+  if (err) {
+    throw new Error(`Error reading filenames at ${ejsDir}`)
+  }
+
+  let ejsFiles = filterEjsFiles(filenames)
+
+  ejsFiles.forEach((ejsFile) => {
+    generateHTML(ejsFile)
+  })
+})
+
+
+
+function filterEjsFiles(filenames) {
+  return filenames.filter((filename) => {
+    if (filename.split('.').pop() === 'ejs') {
+      return true
+    }
+  })
+}
+
+
+
+function generateHTML(ejsFilename) {
+  let htmlFileName = ejsFilename.split('.').shift() + '.html'
+
+  ejs.renderFile(path.join(ejsDir, ejsFilename), {}, (err, html) => {
+    if (err) {
+      throw new Error(`Error creating html from ${ejsFilename}`)
+    }
+
+    fs.writeFile(path.join(htmlDir, htmlFileName), html, (err) => {
+      if (err) {
+        throw new Error(`Error saving ${htmlFileName}`)
+      }
+      console.log(`${htmlFileName} saved..`)
+    })
+  })
+}
---
+++ setup-notes.md
@@ -0,0 +1,12 @@
+# Notes for npm scripts boilerplate
+
+## Build Steps
+* scss to css
+* auto prefix
+* lint
+* test
+* browserify js
+* browsersync
+
+## Do This
+* use sass variables and separate sass file per component
