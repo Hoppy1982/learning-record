29827b9ab86a6d6ba269b233872155d75f1e2bdb
mark hopcraft <mark82hopcraft@gmail.com>
2018-07-31 13:11:16 +0100

    moved some scss from indiv pages to _defaults.scss

---
+++ client/src/pages/node.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html class='no-js' lang='en'>
+
+<head>
+  <%- include('../partials/_head.ejs') %>
+  <link rel='stylesheet' type='text/css' href='node.css'>
+</head>
+
+<body>
+  <div class=page-layout-header>
+    <%- include('../partials/_header.ejs') %>
+    <%- include('../partials/_nav.ejs') %>
+  </div>
+
+
+  <main class='page-layout-main'>
+    <h2 class='page-heading'>Node</h2>
+
+    <section class='node-scripts'>
+
+
+    </section>
+
+  </main>
+
+
+  <div class='page-layout-footer'>
+    <%- include('../partials/_footer.ejs') %>
+  </div>
+</body>
+
+</html>
