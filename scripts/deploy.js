0fda39c53ef9a25fda38fe678907cbbbd8eacb38
mark hopcraft <mark82hopcraft@gmail.com>
2018-07-31 16:19:20 +0100

    added deployment steps in comments of deploy.js script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,3 +13,16 @@ function postPublish(err) {
   if (err) {throw err}
   console.log('deploy script complete..')
 }
+
+/*DEPLOYMENT STEPS
+1. $ git checkout gh-pages
+2. $ npm run build
+3. $ git add .
+4. $ git commit - m "commit msg"
+5. $ git push / git push origin +gh-pages
+6. $ npm run deploy
+
+If it works correctly, home page is rendered at https://hoppy1982.github.io/learning-record/
+If it's gone readme.md is rendered.
+IT MIGHT BE A CACHING THING MARK!!!s CLEAR BROWSER CACHE!!!
+*/
