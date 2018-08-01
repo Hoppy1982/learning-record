2fd0f0833107fe2d3455be5610c1e7d9b1c9ae58
mark hopcraft <mark82hopcraft@gmail.com>
2018-08-01 14:45:38 +0100

    I've been a bad bad man and forgot to commit in ages, mainly tweaked style colors

--- client/src/partials/_nav.js
+++ client/src/partials/_nav.js
@@ -4,10 +4,20 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
 function markCurrentPageInNav() {
   let location = window.location.href
-  console.log(location)
+  let currentPage = location.split('/').pop()
+  let currentPageName = currentPage.split('.').shift()
+  let allPageMkrDivs = document.getElementsByClassName('site-main-nav__page-mkr')
 
-  //splice out current page from full location
-  //get all links from nav
-  //remove class .site-main-nav__page-mkr--current from all of em
-  //add class .site-main-nav__page-mkr--current to current
+  for (let i = 0; i < allPageMkrDivs.length; i++) {
+    let divIdPageName = allPageMkrDivs[i].id.split('-').pop()
+
+    allPageMkrDivs[i].classList.remove('site-main-nav__page-mkr--current')
+
+    if (currentPageName === 'index') {currentPageName = 'home'}
+
+    if (divIdPageName === currentPageName) {
+      allPageMkrDivs[i].classList.add('site-main-nav__page-mkr--current')
+    }
+
+  }
 }
