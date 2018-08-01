a5a5556d1a0a76a6dd2ecfe380c8c8c2e66fc2a7
mark hopcraft <mark82hopcraft@gmail.com>
2018-08-01 16:03:37 +0100

    not update with gh-pages

---
+++ client/src/partials/_nav.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  markCurrentPageInNav()
+  addEventListeners()
+})
+
+
+function addEventListeners() {
+  let navMenuButton = document.getElementById('site-main-nav__menu-button-clickable')
+  navMenuButton.addEventListener('click', function(event) {
+    navMenuButtonOnClick()
+  })
+}
+
+
+function markCurrentPageInNav() {
+  let location = window.location.href
+  let currentPage = location.split('/').pop()
+  let currentPageName = currentPage.split('.').shift()
+  let allPageMkrDivs = document.getElementsByClassName('site-main-nav__page-mkr')
+
+  for (let i = 0; i < allPageMkrDivs.length; i++) {
+    let divIdPageName = allPageMkrDivs[i].id.split('-').pop()
+
+    allPageMkrDivs[i].classList.remove('site-main-nav__page-mkr--current')
+
+    if (currentPageName === 'index') {currentPageName = 'home'}
+
+    if (divIdPageName === currentPageName) {
+      allPageMkrDivs[i].classList.add('site-main-nav__page-mkr--current')
+    }
+
+  }
+}
+
+
+function navMenuButtonOnClick() {
+  let allNavItems = document.getElementsByClassName('site-main-nav__link-item')
+
+  if ( allNavItems[0].classList.contains('site-main-nav__link-item--dontDisplay') ) {
+    for (let i = 0; i < allNavItems.length; i++) {
+      allNavItems[i].classList.remove('site-main-nav__link-item--dontDisplay')
+      allNavItems[i].classList.add('site-main-nav__link-item--display')
+    }
+  } else {
+    for (let i = 0; i < allNavItems.length; i++) {
+      allNavItems[i].classList.remove('site-main-nav__link-item--display')
+      allNavItems[i].classList.add('site-main-nav__link-item--dontDisplay')
+    }
+  }
+
+}
