a5dae27bcc09e5cca33706fcc1edae4502009e9a
mark hopcraft <mark82hopcraft@gmail.com>
2018-08-01 15:57:24 +0100

    got nav menu button working

--- client/src/partials/_nav.js
+++ client/src/partials/_nav.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(event) {
   markCurrentPageInNav()
+  addEventListeners()
 })
+
+
+function addEventListeners() {
+  let navMenuButton = document.getElementById('site-main-nav__menu-button-clickable')
+  navMenuButton.addEventListener('click', function(event) {
+    navMenuButtonOnClick()
+  })
+}
+
 
 function markCurrentPageInNav() {
   let location = window.location.href
@@ -20,4 +30,22 @@ function markCurrentPageInNav() {
     }
 
   }
+}
+
+
+function navMenuButtonOnClick() {
+  let allNavItems = document.getElementsByClassName('site-main-nav__link-item')
+
+  if ( allNavItems[0].classList.contains('site-main-nav__link-item--dontDisplay') ) {
+    for (let i = 0; i < allNavItems.length; i++) {
+      allNavItems[i].classList.remove('site-main-nav__link-item--dontDisplay')
+      allNavItems[i].classList.add('site-main-nav__link-item--display')
+    }
+  } else {
+    for (let i = 0; i < allNavItems.length; i++) {
+      allNavItems[i].classList.remove('site-main-nav__link-item--display')
+      allNavItems[i].classList.add('site-main-nav__link-item--dontDisplay')
+    }
+  }
+
 }
