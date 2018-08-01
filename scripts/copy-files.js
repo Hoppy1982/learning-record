3d02833d748a7a4c501365b5b8d3d50b938fc51c
mark hopcraft <mark82hopcraft@gmail.com>
2018-08-01 13:17:44 +0100

    modified copy-files.js to be more generic, now contains an array of copyDetailsObjs, each copyDetailsObj specifies src, dest and ext

---
+++ scripts/copy-files.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const fs = require('fs')
+const shelljs = require('shelljs')
+
+
+const toCopy = [
+  {
+    name: 'favicon stuffs',
+    src: path.join(__dirname, '/../client/src/static/toRoot'),
+    dest: path.join(__dirname, '/../client/build'),
+    ext: '*'
+  },
+  {
+    name: 'images',
+    src: path.join(__dirname, '/../client/src/static/images'),
+    dest: path.join(__dirname, '/../client/build/static/images'),
+    ext: 'jpg'
+  }
+]
+
+
+toCopy.forEach(copyDetailsObj => {
+  let filteredFiles
+
+  fs.readdir(copyDetailsObj.src, (err, filenames) => {
+    if (err) { throw new Error(`Error reading filenames at ${copyDetailsObj.src}`) }
+
+    shelljs.mkdir('-p', copyDetailsObj.dest)//fs can only make one subdir at a time
+    filteredFiles = filterFiles(filenames, copyDetailsObj.ext)
+    copyFiles(filteredFiles, copyDetailsObj.src, copyDetailsObj.dest)
+  })
+
+})
+
+
+function filterFiles(filenames, ext) {
+  return filenames.filter(filename => {
+    if (ext === '*') {return true}
+    if (filename.split('.').pop() === ext) {return true}
+  })
+}
+
+
+function copyFiles(filenames, src, dest) {
+  filenames.forEach(filename => {
+    fs.copyFile(`${src}/${filename}`, `${dest}/${filename}`, (err) => {
+      console.log(src)
+      console.log(dest)
+      console.log(filename)
+      if (err) {
+        throw new Error(`Error copying ${filename}`)
+      }
+
+      console.log(`${filename} copied to ${dest}`)
+    })
+  })
+}
