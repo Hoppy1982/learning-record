86581a6902ce2f11fba92cb39b955e3d5cf4b5e6
mark hopcraft <mark82hopcraft@gmail.com>
2018-08-07 13:06:09 +0100

    altered express server to work with html files, had to set client/build as a static dir

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const SERVERCONFIG= require(path.join(__dirname, 'server/server-config'))
 const ROUTESINDEX = require(path.join(__dirname, 'server/routes/index'))
 
 var server = express()
-
+/*
 server.set('view engine', 'ejs')
 server.set('views', path.join(__dirname, 'client/build'))
-server.use(express.static(path.join(__dirname, 'client/build/static')))
+*/
+server.use(express.static(path.join(__dirname, 'client/build')))
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({extended: false}))
 server.use('/', ROUTESINDEX)
